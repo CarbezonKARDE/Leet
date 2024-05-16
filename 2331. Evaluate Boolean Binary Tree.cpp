a0f1445880c0edadaaa3caef6a85f783e50eb7f8
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-05-16 15:13:37 +0530

    Create 2331. Evaluate Boolean Binary Tree.cpp

---
+++ 2331. Evaluate Boolean Binary Tree.cpp
@@ -0,0 +1,10 @@
+class Solution {
+ public:
+  bool evaluateTree(TreeNode* root) {
+    if (root->val < 2)
+      return root->val;
+    if (root->val == 2)
+      return evaluateTree(root->left) || evaluateTree(root->right);
+    return evaluateTree(root->left) && evaluateTree(root->right);
+  }
+};
