042f6540b27be32d365fa308b23a6c3fb6e8fb45
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-06-13 11:04:13 +0530

    Create 2037. Minimum Number of Moves to Seat Everyone.rs

---
+++ 2037. Minimum Number of Moves to Seat Everyone.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn min_moves_to_seat(mut seats: Vec<i32>, mut students: Vec<i32>) -> i32 {
+        seats.sort();
+        students.sort();
+        seats.iter()
+            .zip(students.iter())
+            .map(|(seat, student)| (seat - student).abs())
+            .sum()
+    }
+}
