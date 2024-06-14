33ecf02150f6595935d4df377764d33c8f64b20b
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-06-14 15:59:10 +0530

    Create 945. Minimum Increment to Make Array Unique.cpp

---
+++ 945. Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,13 @@
+class Solution {
+ public:
+  int minIncrementForUnique(vector<int>& nums) {
+    int ans = 0;
+    int minAvailable = 0;
+    ranges::sort(nums);
+    for (const int num : nums) {
+      ans += max(minAvailable - num, 0);
+      minAvailable = max(minAvailable, num) + 1;
+    }
+    return ans;
+  }
+};
