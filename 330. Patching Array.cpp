de0ab15e542f8e063ac4e7fc35e5ecf4435fad97
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-06-16 14:13:29 +0530

    Create 330. Patching Array.cpp

---
+++ 330. Patching Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  int minPatches(vector<int>& nums, int n) {
+    int ans = 0;
+    int i = 0;
+    long miss = 1;
+    while (miss <= n)
+      if (i < nums.size() && nums[i] <= miss) {
+        miss += nums[i++];
+      } else {
+        miss += miss;
+        ++ans;
+      }
+    return ans;
+  }
+};
