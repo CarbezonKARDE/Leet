ec3c5dd54daabe184e8ff1c57041d4be769dc1f6
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-06-20 18:59:34 +0530

    Create 1552. Magnetic Force Between Two Balls.cpp

---
+++ 1552. Magnetic Force Between Two Balls.cpp
@@ -0,0 +1,27 @@
+class Solution {
+ public:
+  int maxDistance(vector<int>& position, int m) {
+    ranges::sort(position);
+    int l = 1;
+    int r = position.back() - position.front();
+    while (l < r) {
+      const int mid = r - (r - l) / 2;
+      if (numBalls(position, mid) >= m)
+        l = mid;
+      else
+        r = mid - 1;
+    }
+    return l;
+  }
+ private:
+  int numBalls(const vector<int>& position, int force) {
+    int balls = 0;
+    int prevPosition = -force;
+    for (const int pos : position)
+      if (pos - prevPosition >= force) {
+        ++balls;
+        prevPosition = pos;
+      }
+    return balls;
+  }
+};
