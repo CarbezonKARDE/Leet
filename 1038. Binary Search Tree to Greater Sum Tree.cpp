c8e0f0fc72fba46e28244cfbc445e806c46c1dcf
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-06-25 15:45:51 +0530

    Create 1038. Binary Search Tree to Greater Sum Tree.cpp

---
+++ 1038. Binary Search Tree to Greater Sum Tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  TreeNode* bstToGst(TreeNode* root) {
+    int prefix = 0;
+    function<void(TreeNode*)> reversedInorder = [&](TreeNode* root) {
+      if (root == nullptr)
+        return;
+      reversedInorder(root->right);
+      root->val += prefix;
+      prefix = root->val;
+      reversedInorder(root->left);
+    };
+    reversedInorder(root);
+    return root;
+  }
+};
