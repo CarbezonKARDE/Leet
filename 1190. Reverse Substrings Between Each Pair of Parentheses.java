3ee3846538a3e8387b324ca88f204a16fe38c180
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-07-11 16:38:23 +0530

    Create 1190. Reverse Substrings Between Each Pair of Parentheses.java

---
+++ 1190. Reverse Substrings Between Each Pair of Parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+  public String reverseParentheses(String s) {
+    Deque<Integer> stack = new ArrayDeque<>();
+    StringBuilder sb = new StringBuilder();
+    for (final char c : s.toCharArray())
+      if (c == '(') {
+        stack.push(sb.length());
+      } else if (c == ')') {
+        StringBuilder reversed = new StringBuilder();
+        for (int sz = sb.length() - stack.pop(); sz > 0; --sz) {
+          reversed.append(sb.charAt(sb.length() - 1));
+          sb.deleteCharAt(sb.length() - 1);
+        }
+        sb.append(reversed);
+      } else {
+        sb.append(c);
+      }
+    return sb.toString();
+  }
+}
