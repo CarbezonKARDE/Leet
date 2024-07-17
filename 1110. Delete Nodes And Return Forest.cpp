0516d67f7257e61939410fe362c15bbfb3b2a423
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-07-17 14:44:36 +0530

    Create 1110. Delete Nodes And Return Forest.cpp

---
+++ 1110. Delete Nodes And Return Forest.cpp
@@ -0,0 +1,20 @@
+class Solution {
+ public:
+  vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+    vector<TreeNode*> ans;
+    dfs(root, {to_delete.begin(), to_delete.end()}, true, ans);
+    return ans;
+  }
+ private:
+  TreeNode* dfs(TreeNode*& root, const unordered_set<int>&& toDeleteSet,
+                bool isRoot, vector<TreeNode*>& ans) {
+    if (root == nullptr)
+      return nullptr;
+    const bool deleted = toDeleteSet.count(root->val);
+    if (isRoot && !deleted)
+      ans.push_back(root);
+    root->left = dfs(root->left, move(toDeleteSet), deleted, ans);
+    root->right = dfs(root->right, move(toDeleteSet), deleted, ans);
+    return deleted ? nullptr : root;
+  }
+};
