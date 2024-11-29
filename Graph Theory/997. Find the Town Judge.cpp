5ca85bbd35cbea94fd2c2f12c4f1e54a7a9f97ef
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-11-29 16:56:19 +0530

    Create 997. Find the Town Judge.cpp

---
+++ Graph Theory/997. Find the Town Judge.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    int findJudge(int N,vector<vector<int>>&trust)
+    {
+        vector<pair<int,int>>arr(N+1,{0,0});
+        for(int i=0;i<trust.size();i++)
+        {
+            arr[trust[i][0]].first++;
+            arr[trust[i][1]].second++;
+        }
+        for(int i=1;i<=N;i++)
+        {
+            if(arr[i].first==0 && arr[i].second==N-1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
