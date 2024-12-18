b389958d57c199b53fc2851234efbba70e44242c
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2024-12-18 15:43:33 +0530

    Create 1475. Final Prices With a Special Discount in a Shop.java

---
+++ 1475. Final Prices With a Special Discount in a Shop.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = prices[i];
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    res[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
