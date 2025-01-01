71f31355bb4fa54ecb5f0638492c6daa83e1f4da
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-01-01 09:56:27 +0530

    Create 1422. Maximum Score After Splitting a String.java

---
+++ 1422. Maximum Score After Splitting a String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxScore(String s) {
+        int n = s.length();
+        int ones = 0;
+        for (int i = 0; i < n; i++)
+            if (s.charAt(i) == '1')
+                ones++;
+        int zeros = 0;
+        int maxScore = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == '0') 
+                zeros++;
+            else
+                ones--;
+            maxScore = Math.max(maxScore, zeros + ones);
+        }
+        return maxScore;
+    }
+}
