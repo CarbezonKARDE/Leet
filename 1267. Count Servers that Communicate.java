87bbacb95d4c016b6a77cc23743c3db4ff506104
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-01-23 14:07:54 +0530

    Create 1267. Count Servers that Communicate.java

---
+++ 1267. Count Servers that Communicate.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countServers(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int[] rowCnt=new int[m]; 
+        int[] colCnt=new int[n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1){                   
+                    rowCnt[i]++;
+                    colCnt[j]++;                    
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1){                   
+                    if(rowCnt[i]>1 || colCnt[j]>1){
+                        cnt++;
+                    }                 
+                }
+            }
+        }
+        return cnt;
+    }
+}
