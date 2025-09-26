c41a48fdded61ddae07f9792af65e30eb0c253fe
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-09-26 15:56:51 +0530

    Create 611. Valid Triangle Number.rs

---
+++ 611. Valid Triangle Number.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn triangle_number(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        let mut count = 0;
+        nums.sort_unstable();
+        for k in (2..nums.len()).rev() {
+            let mut i = 0;
+            let mut j = k - 1;
+            while i < j {
+                if nums[i] + nums[j] > nums[k] {
+                    count += j - i;
+                    j -= 1;
+                } else {
+                    i += 1;
+                }
+            }
+        }
+        count as i32
+    }
+}
