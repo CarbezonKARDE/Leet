a2cf093d10d678c2b3c01dc18b0e1ef7dca2ead7
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-04-21 13:34:44 +0530

    Create 1722. Minimize Hamming Distance After Swap Operations.cpp

---
+++ 1722. Minimize Hamming Distance After Swap Operations.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> parent;
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    void unite(int a, int b) {
+        parent[find(a)] = find(b);
+    }
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int n = source.size();
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+        for (auto& swap : allowedSwaps) {
+            unite(swap[0], swap[1]);
+        }
+        unordered_map<int, unordered_map<int, int>> groups;
+        for (int i = 0; i < n; i++) {
+            groups[find(i)][source[i]]++;
+        }
+        int hammingDist = 0;
+        for (int i = 0; i < n; i++) {
+            int root = find(i);
+            auto& freq = groups[root];
+            if (freq.count(target[i]) && freq[target[i]] > 0) {
+                freq[target[i]]--;
+            } else {
+                hammingDist++;
+            }
+        }
+        return hammingDist;
+    }
+};
