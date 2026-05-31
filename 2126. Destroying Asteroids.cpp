3c0c27f31dd333282d02d1be327e3f4e85f79347
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-05-31 09:44:30 +0530

    Create 2126. Destroying Asteroids.cpp

---
+++ 2126. Destroying Asteroids.cpp
@@ -0,0 +1,28 @@
+unsigned freq[100001]={0};
+class Solution {
+public:
+    static bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        unsigned xmax=0;
+        for(unsigned x: asteroids){
+            freq[x]++;
+            xmax=max(xmax, x);
+        }
+        long long planet=mass;
+        for(int x=1; x<=xmax; x++){
+            if (freq[x]==0) continue;
+            if (x>planet) {
+                memset(freq+x, 0, (xmax-x+1)*sizeof(unsigned));
+                return 0;
+            }
+            planet+=(long long)x*freq[x];
+            freq[x]=0;
+        }
+        return 1;
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
