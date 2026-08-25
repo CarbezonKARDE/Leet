af665c68008ef44f70c9a2276cf116b953779c54
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2026-08-25 15:44:51 +0530

    Create 3718. Smallest Missing Multiple of K.cpp

---
+++ 3718. Smallest Missing Multiple of K.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> seen(nums.begin(), nums.end());
+        int cur = k;
+        while (seen.count(cur)) {
+            cur += k;
+        }
+        return cur;
+    }
+};
